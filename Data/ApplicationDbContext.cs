523b36b6161c2a3fce66d9870c93d6dfc61f7e42
ma224ip <33762491+ma224ip@users.noreply.github.com>
2025-02-17 10:52:35 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Cybergames.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cybergames
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        // Define your DbSet properties here
+        // For example:
+        // public DbSet<YourModel> YourModels { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var admin = new IdentityRole("admin");
+            admin.NormalizedName = "admin";
+            
+            var clinet = new IdentityRole("client ");
+            clinet.NormalizedName = "client";
+
+            builder.Entity<IdentityRole>().HasData(admin,clinet);
+
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }
+    }
+
+    //public class ApplicationDbContext : DbContext
+    //{
+    //    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    //        : base(options)
+    //    {
+    //    }
+
+    //    public DbSet<Game> Games { get; set; }
+    //}
+}
